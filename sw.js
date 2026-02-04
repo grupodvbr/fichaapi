db0031916491b96439defd4dc5fcfce7583cf2b5
grupodvbr <grupodvbr@gmail.com>
2026-02-04 19:52:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
-self.addEventListener("install", (e) => {
+const CACHE_NAME = "cardapio-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  self.clients.claim();
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
-
-self.addEventListener("fetch", () => {});
