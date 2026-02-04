8dc67df59e8cb5cc0e2e282a3e3e234bb9abfccc
grupodvbr <grupodvbr@gmail.com>
2026-02-04 17:20:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,3 @@
-const CACHE = "cardapio-v1";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/manifest.json"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => self.clients.claim());
+self.addEventListener("fetch", () => {});
