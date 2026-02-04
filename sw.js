6daed60a66601ab76690af2d3870098149d6bbdb
grupodvbr <grupodvbr@gmail.com>
2026-02-04 14:01:10 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "cardapio-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
